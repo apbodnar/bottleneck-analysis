b332e806d9b96ec17b0bedd6d7a13bb29c9d27cd
Adam Bodnar <abodnar@google.com>
2020-09-17 16:32:07 -0700

    be gentler to sql

--- analyze.py
+++ analyze.py
@@ -28,68 +28,59 @@ def flatten_slices(slices):
 
 def get_frame_time_stats(tp, package_name, api):
     # Find the frame interval
-    stats = {'histogram': {}, 'busy_histogram': {}}
-    sum = 0
+    stats = {'busy_sum_frametimes': {}, 'busy_span_frametimes': {}}
+    QUEUE_QUERY = "SELECT s.name, s.ts, s.dur, g.submission_id FROM slices s \
+        LEFT JOIN gpu_slice g ON g.name=s.name AND s.ts=g.ts AND s.dur=g.dur \
+        WHERE s.name='vkQueuePresentKHR' or (g.submission_id IS NOT NULL and g.name='vkQueueSubmit') \
+        ORDER BY s.ts"
+    GPU_SLICE_QUERY = "SELECT * FROM gpu_slice WHERE name!='vkQueueSubmit' and submission_id IS NOT NULL ORDER BY ts"
+    busy_sum = 0
+    busy_span_sum = 0
     count = 0
-    diffs = []
-    GPU_SLICE_QUERY = "SELECT ts, dur, submission_id FROM gpu_slice WHERE NOT name='vkQueueSubmit' AND submission_id IS NOT NULL"
     intervals = []
     submission_slices = {}
+    submit_slices = []
     if api == "Vulkan":
-        # find all vkQueuePresent calls, and all vkQueueSubmit call between them
-        interval_query = "SELECT ts FROM slices WHERE name='vkQueuePresentKHR' ORDER BY ts"
-        interval_query_it = tp.query(interval_query)
-        if len(interval_query_it.cells()) == 0:
-            print("Interval query returned no vkQueuePresentKHR slices, trying a fuzzy search")
-            interval_query = "SELECT ts FROM slices WHERE name LIKE '%queuepresent%' ORDER BY ts"
-            interval_query_it = tp.query(interval_query)
+        # do a rather cumbersome enumeration to avoid slamming the database
         slices_query_it = tp.query(GPU_SLICE_QUERY)
         for slice_row in slices_query_it:
             if slice_row.submission_id not in submission_slices:
                 submission_slices[slice_row.submission_id] = [{'ts': slice_row.ts, 'dur': slice_row.dur}]
             else:
                 submission_slices[slice_row.submission_id].append({'ts': slice_row.ts, 'dur': slice_row.dur})
-        ts_begin = -1
-        ts_end_= -1
+        # find all vkQueuePresent calls, and all vkQueueSubmit call between them
         # iterate frame intervals
-        for i, interval_row in enumerate(interval_query_it):
-            if i == 0:
-                ts_end = interval_row.ts
-            else:
-                frame_slices = []
-                ts_begin = ts_end
-                ts_end = interval_row.ts
-                #SELECT * FROM slices s JOIN track t ON t.id=s.track_id WHERE s.name='vkQueuePresentKHR' ORDER BY ts
-                submit_query = "SELECT submission_id FROM gpu_slice WHERE name='vkQueueSubmit' AND ts>={} AND ts<{} ORDER BY ts".format(ts_begin, ts_end)
-                submit_query_it = tp.query(submit_query)
+        queue_query_it = tp.query(QUEUE_QUERY)
+        for queue_row in queue_query_it:
+            print(queue_row.name)
+            if queue_row.name == 'vkQueuePresentKHR':
+                intervals.append([])
+            if len(intervals) > 0 and queue_row.submission_id in submission_slices and queue_row.name != 'vkQueuePresentKHR':
+                intervals[-1] += submission_slices[queue_row.submission_id]
+
+        for interval in intervals:
+            if len(interval) > 0:
+                count += 1
+                flattened_slices = flatten_slices(interval)
+                active_time = sum(map(lambda s: s['dur'], flattened_slices))
                 range_max = -math.inf
                 range_min = math.inf
                 # iterate submissions
-                for submit_row in submit_query_it:
-                    # iterate slices
-                    if submit_row.submission_id in submission_slices:
-                        for slice in submission_slices[submit_row.submission_id]:
-                            frame_slices.append(slice)
-                            range_max = max(range_max, slice['ts'] + slice['dur'])
-                            range_min = min(range_min, slice['ts'])
-                diff = range_max - range_min
-                sum += diff
-                count += 1
-                flattened = flatten_slices(frame_slices)
-                flat_sum = 0
-                for slice in flattened:
-                    flat_sum += slice['dur']
-                key = int(flat_sum / 1000000)
-                if key not in stats['busy_histogram']:
-                    stats['busy_histogram'][key] = 1
+                for gpu_slice in interval:
+                    range_max = max(range_max, gpu_slice['ts'] + gpu_slice['dur'])
+                    range_min = min(range_min, gpu_slice['ts'])
+                span_time = range_max - range_min
+                key = int(active_time / 1000000)
+                if key not in stats['busy_sum_frametimes']:
+                    stats['busy_sum_frametimes'][key] = 1
                 else:
-                    stats['busy_histogram'][key] += 1
-                key = int(diff / 1000000)
-                if key not in stats['histogram']:
-                    stats['histogram'][key] = 1
+                    stats['busy_sum_frametimes'][key] += 1
+                key = int(span_time / 1000000)
+                if key not in stats['busy_span_frametimes']:
+                    stats['busy_span_frametimes'][key] = 1
                 else:
-                    stats['histogram'][key] += 1
-        
+                    stats['busy_span_frametimes'][key] += 1
+
     elif api == "GL":
         # this own't work until we have more actionable submission info from vendors
         # interval_query = "SELECT * FROM slices WHERE name LIKE 'eglSwapBuffers'"
@@ -98,15 +89,23 @@ def get_frame_time_stats(tp, package_name, api):
         raise ValueError("Bad api")
 
     # Compute the median
-    keys = sorted(stats['histogram']) 
+    keys = sorted(stats['busy_sum_frametimes']) 
     median = 0
     for key in keys:
-        median += stats['histogram'][key]
+        median += stats['busy_sum_frametimes'][key]
         if median >= count / 2:
-            stats['median'] = key
+            stats['busy_sum_median'] = key
             break
 
-    stats['mean'] = (sum / count) / 1000000
+    keys = sorted(stats['busy_span_frametimes']) 
+    median = 0
+    for key in keys:
+        median += stats['busy_span_frametimes'][key]
+        if median >= count / 2:
+            stats['busy_span_median'] = key
+            break
+
+    # stats['mean'] = (sum / count) / 1000000
     return stats
 
 def analyze_query(query_result):
